1ccf82fc94a169fd3186707a2d1302de9e524361
maizhiheng <maizhiheng1993@gmail.com>
2025-04-18 14:02:32 +0800

    新增电源管理，内存优化

--- main/iot/things/battery.cc
+++ main/iot/things/battery.cc
@@ -2,8 +2,10 @@
 #include "board.h"
 
 #include <esp_log.h>
+#include <esp_timer.h>
 
 #define TAG "Battery"
+#define BATTERY_UPDATE_INTERVAL_MS 30000  // 30秒更新一次
 
 namespace iot {
 
@@ -13,18 +15,31 @@ private:
     int level_ = 0;
     bool charging_ = false;
     bool discharging_ = false;
+    int64_t last_update_time_ = 0;  // 上次更新时间
+
+    // 在需要时更新电池状态
+    void UpdateBatteryStatusIfNeeded() {
+        int64_t current_time = esp_timer_get_time() / 1000;  // 转换为毫秒
+        
+        // 如果距离上次更新超过指定间隔，则更新状态
+        if (current_time - last_update_time_ >= BATTERY_UPDATE_INTERVAL_MS) {
+            auto& board = Board::GetInstance();
+            board.GetBatteryLevel(level_, charging_, discharging_);
+            last_update_time_ = current_time;
+            ESP_LOGI(TAG, "1111 Battery level updated: %d%%, Charging: %s", 
+                     level_, charging_ ? "Yes" : "No");
+        }
+    }
 
 public:
     Battery() : Thing("Battery", "电池管理") {
         // 定义设备的属性
         properties_.AddNumberProperty("level", "当前电量百分比", [this]() -> int {
-            auto& board = Board::GetInstance();
-            if (board.GetBatteryLevel(level_, charging_, discharging_)) {
-                return level_;
-            }
-            return 0;
+            UpdateBatteryStatusIfNeeded();
+            return level_;
         });
         properties_.AddBooleanProperty("charging", "是否充电中", [this]() -> int {
+            UpdateBatteryStatusIfNeeded();
             return charging_;
         });
     }
